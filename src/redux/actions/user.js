7504163a10243cff76b3487e79bb538ce28f0e45
SEJAL JAIN <69568063+sejal-jain-20@users.noreply.github.com>
2023-06-29 23:57:23 +0530

    Updated user.js

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -45,13 +45,13 @@ export const loadUser = () => async dispatch => {
   try {
     dispatch({ type: 'loadUserRequest' });
 
-    const { data } = await axios.get(`${server}/me`, {
-      withCredentials: true,
-      headers: {
-        'Access-Control-Allow-Origin': 'https://course-bundler-frontend-five.vercel.app',
-      },
-    });
+    const { data } = await axios.get(
+      `${server}/me`,
 
+      {
+        withCredentials: true,
+      }
+    );
     dispatch({ type: 'loadUserSuccess', payload: data.user });
   } catch (error) {
     dispatch({ type: 'loadUserFail', payload: error.response.data.message });
